3dc33c55e3dd441f5df1a0d5d4b868f3edb69ac4
Egor <egorgolev702@gmail.com>
2024-09-10 23:05:59 +0300

    migrate from random to secrets lib

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import argparse
-import random
+import secrets
 import string
 
 
-def get_letters(special_chars: bool) -> list[str]:
+def get_letters(rnd_object: secrets.SystemRandom, special_chars: bool) -> list[str]:
     punctuation = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~'
     letters = string.ascii_letters + string.digits
     if special_chars: letters += punctuation
     lst_letters = list(letters)
-    random.shuffle(lst_letters)
+    rnd_object.shuffle(lst_letters)
     return lst_letters
 
 
-def generate_password(length: int = 8, special_chars: bool = True) -> str:
-    letters = get_letters(special_chars)
-    passwd = random.choices(letters, k=length)
+def generate_password(rnd_object: secrets.SystemRandom, length: int = 8, special_chars: bool = True) -> str:
+    letters = get_letters(rnd_object, special_chars)
+    passwd = rnd_object.choices(letters, k=length)
     return ''.join(passwd)
 
 
@@ -24,7 +24,8 @@ def main():
     parser.add_argument('--special', '-s', action='store_true', help='Include special characters')
     
     args = parser.parse_args()
-    passwd = generate_password(args.length, args.special)
+    rnd = secrets.SystemRandom()
+    passwd = generate_password(rnd, args.length, args.special)
     print(passwd)
 
 
